2449d422aa1467889177518608aeb22e7fbc607e
bcwongaa <bcwongaa@gmail.com>
2024-07-10 22:12:40 +0800

    nonNull on BookDto

--- src/main/java/com/roman/bookmanagementsystem/dtos/BookDto.java
+++ src/main/java/com/roman/bookmanagementsystem/dtos/BookDto.java
@@ -2,12 +2,19 @@ package com.roman.bookmanagementsystem.dtos;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NonNull;
 
 @Setter
 @Getter
 public class BookDto {
+
     public Long id;
+
+    @NonNull
     public String title;
+    @NonNull
     public String author;
+    @NonNull
     public Boolean published;
 }
+
